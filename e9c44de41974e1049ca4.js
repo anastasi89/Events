ceb810e62edec8f7cf8be797c2cb0d599b6806b4
Внукова Анастасия <nastenka696945@gmail.com>
2023-08-24 19:34:48 +0300

    Updates

---
+++ e9c44de41974e1049ca4.js
@@ -0,0 +1,1 @@
+import GameManager from"./gameManager";import EventsManager from"./eventsManager";const gameManager=new GameManager,eventsManager=new EventsManager(gameManager);eventsManager.init();
